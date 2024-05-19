098808f48c7b30ff632b6aa668f59d5aa3cdbd69
Carlos Barrio <cbr1005@alu.ubu.es>
2024-05-19 20:49:59 +0200

    Creacion de metodo calculateFrequency

--- src/ubu/gii/dass/refactoring/Customer.java
+++ src/ubu/gii/dass/refactoring/Customer.java
@@ -41,12 +41,7 @@ public class Customer {
 			// determine amounts for each line
 			thisAmount = calculateAmount(thisAmount, each);
 			
-			// add frequent renter points
-			frequentRenterPoints++;
-			// add bonus for a two day new release rental
-			if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
-					&& each.getDaysRented() > 1)
-				frequentRenterPoints++;
+			frequentRenterPoints = calculateFrequency(frequentRenterPoints, each);
 			// show figures for this rental
 			result += "\t" + each.getMovie().getTitle() + "\t"
 					+ String.valueOf(thisAmount) + "\n";
@@ -57,6 +52,16 @@ public class Customer {
 		result += "You earned " + String.valueOf(frequentRenterPoints)
 				+ " frequent renter points";
 		return result;
+	}
+
+	public int calculateFrequency(int frequentRenterPoints, Rental each) {
+		// add frequent renter points
+		frequentRenterPoints++;
+		// add bonus for a two day new release rental
+		if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
+				&& each.getDaysRented() > 1)
+			frequentRenterPoints++;
+		return frequentRenterPoints;
 	}
 
 	public double calculateAmount(double thisAmount, Rental each) {
